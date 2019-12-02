31fa231ab389104597a1c21a61c28363d8463887
Zi Li <820972562@qq.com>
2019-12-02 11:30:22 +0800

    add compiler writer

--- src/main/scala/com/okg/actor/StatisticsActor.scala
+++ src/main/scala/com/okg/actor/StatisticsActor.scala
@@ -1,6 +1,7 @@
 package com.okg.actor
 
 import java.io.File
+import java.nio.file.Files
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import com.csvreader.CsvWriter
@@ -13,7 +14,8 @@ import scala.concurrent.duration.Duration
 class StatisticsActor(instanceActors: Array[ActorRef]) extends Actor with ActorLogging {
 
   val instanceSize = instanceActors.size
-  val periodWriters = new Array[CsvWriter](instanceSize)
+  val instanceWriters = new Array[CsvWriter](instanceSize)
+  var compilerWriter: CsvWriter = null
 
   var instanceResultDirectory: File = new File("instance_statistics_output")
 
@@ -28,9 +30,15 @@ class StatisticsActor(instanceActors: Array[ActorRef]) extends Actor with ActorL
       }
     }
     for (i <- 0 to instanceSize - 1) {
-      val fileName = instanceResultDirectory.getCanonicalPath + "/instance_" + i + ".csv"
-      periodWriters(i) = new CsvWriter(fileName)
+      val instanceFileName = instanceResultDirectory.getCanonicalPath + "/instance_" + i + ".csv"
+      instanceWriters(i) = new CsvWriter(instanceFileName)
     }
+    val compilerFileName = "compiler_output.csv"
+    val compilerFile = new File(compilerFileName)
+    if(compilerFile.exists()) {
+      compilerFile.delete()
+    }
+    compilerWriter = new CsvWriter(compilerFileName)
   }
 
   var receivedTupleSum = 0
@@ -46,33 +54,41 @@ class StatisticsActor(instanceActors: Array[ActorRef]) extends Actor with ActorL
     }
 
     case InstanceStatistics(index, period, periodTuplesNum, totalTuplesNum) => {
-      log.info("Statistic: instance " + index + " at " + period + " received "
-        + periodTuplesNum + ", " + totalTuplesNum + " in total")
+      log.info("Statistic: instance " + index + " received "
+        + periodTuplesNum + " at period " + period + ", " + totalTuplesNum + " in total")
       instancesNum += 1
       receivedTupleSum += periodTuplesNum
       if (instancesNum == instanceActors.length) {
         log.info("Statistic: all the instances received " + receivedTupleSum + " so far")
         instancesNum = 0
       }
-      val record = new Array[String](2)
-      record(0) = period.toString
-      record(1) = totalTuplesNum.toString
-      periodWriters(index).writeRecord(record)
-      periodWriters(index).flush()
+      val instanceRecord = new Array[String](2)
+      instanceRecord(0) = period.toString
+      instanceRecord(1) = totalTuplesNum.toString
+      instanceWriters(index).writeRecord(instanceRecord)
+      instanceWriters(index).flush()
 
     }
+
     case CompilerStatistics(period: Int,
                             routingTableGenerationTime: Duration,
                             routingTableSize: Int,
                             migrationTableSize: Int) => {
-      
-
+      log.info("Statistic: compiler takes " + routingTableGenerationTime + " to generate next routing table"
+        + " at period " + period + ", " + " its size is " + routingTableSize + ", " + "migration table size is "
+        + migrationTableSize)
+      val compilerRecord = new Array[String](3)
+      compilerRecord(0) = routingTableGenerationTime.toString
+      compilerRecord(1) = routingTableSize.toString
+      compilerRecord(2) = migrationTableSize.toString
+      compilerWriter.writeRecord(compilerRecord)
+      compilerWriter.flush()
     }
 
   }
 
   override def postStop(): Unit = {
-    periodWriters.foreach {
+    instanceWriters.foreach {
       periodWriter => {
         periodWriter.close()
       }
